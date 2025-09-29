4670a2338bcb65aaf8ec6906b3633a37f48043cc
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-09-29 23:23:38 +0200

    reducing privileges instead of elevating privileges

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
-
     mirage = {
       url = "github:FlorianNAdam/mirage";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -30,6 +28,10 @@
           mirage-patch = pkgs.writeShellScriptBin "mirage-patch" ''
             set -e
 
+            if [ "$EUID" -ne 0 ]; then
+              echo "error: this script must be run as root" >&2
+              exit 1
+            fi
             if [ "$#" -ne 1 ]; then
               echo "Usage: $0 <path-to-file>"
               exit 1
@@ -40,19 +42,23 @@
               exit 1
             fi
 
+            # Create Tempfile
             temp_file=$(mktemp /tmp/tempfile.XXXXXX)
             cp -- "$FILE" "$temp_file"
+            chown "$SUDO_USER" "$temp_file"
 
-            sudo true
-            sudo ${
+            # Start mirage in background
+            ${
               mirage.defaultPackage.${pkgs.system}
             }/bin/mirage --exec "cat $temp_file" --allow-other "$FILE" > /dev/null 2>&1 &
             mirage_pid=$!
 
-            "''${EDITOR:-nano}" "$temp_file"
+            # Run editor as the original user using su -l
+            su -l "$SUDO_USER" -c '"''${EDITOR:-nano}" '"$temp_file"
 
-            sudo kill "$mirage_pid" 2>/dev/null
-            wait "$mirage_pid" 2>/dev/null
+            # Kill background mirage process
+            kill "$mirage_pid" 2>/dev/null || true
+            wait "$mirage_pid" 2>/dev/null || true
 
             rm -f "$temp_file"
           '';
